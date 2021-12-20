21ed5c8cea0bdc140cce61fef6f87eb28181c3a2
spapa013 <stelios@spapa.us>
2021-12-20 11:14:51 -0600

    adds DataJointPlusModule and alias create_djp_module

--- datajoint/datajoint_plus.py
+++ datajoint/datajoint_plus.py
@@ -23,7 +23,8 @@ from ipywidgets.widgets import Output, HBox, Label
 import traceback
 import copy
 import logging
-
+from .table import FreeTable
+from .user_tables import UserTable
 
 __version__ = "0.0.17"
 
@@ -777,7 +778,7 @@ class MasterBase(Base):
         parts = cls.restrict_parts(part_restr=part_restr, include_parts=include_parts, exclude_parts=exclude_parts, filter_out_len_zero=filter_out_len_zero, parts_kws=parts_kws)
         
         if join_with_master:
-            parts = [dj.FreeTable(cls.connection, cls.full_table_name)] + parts
+            parts = [FreeTable(cls.connection, cls.full_table_name)] + parts
 
         collisions = None
         if join_method is None:
@@ -1177,20 +1178,22 @@ def add_datajoint_plus(module):
     """
     Adds DataJointPlus recursively to DataJoint tables inside the module.
     """
-    for name in dir(module):
-        try:
-            if inspect.isclass(getattr(module, name)) and issubclass(getattr(module, name), dj.Table) and not issubclass(getattr(module, name), Base):
-                obj = getattr(module, name)
+    try:
+        for name in dir(module):
+            if name in ['key_source', '_master', 'master']:
+                continue
+            obj = getattr(module, name)
+            if inspect.isclass(obj) and issubclass(obj, UserTable) and not issubclass(obj, Base):
                 bases = []
                 for b in obj.__bases__:
-                    if issubclass(b, dj.Table):
+                    if issubclass(b, UserTable):
                         b = djp_mapping[b.__name__]
                     bases.append(b)
                 obj.__bases__ = tuple(bases)
                 add_datajoint_plus(obj)
-        except:
-            logging.warning(f'Could not add DataJointPlus to: {name}.')
-            traceback.print_exc()
+    except:
+        logging.warning(f'Could not add DataJointPlus to: {name}.')
+        traceback.print_exc()
 
 
 def reassign_master_attribute(module):
@@ -1205,3 +1208,75 @@ def reassign_master_attribute(module):
                 # Get Part tables
                 if inspect.isclass(getattr(obj, nested)) and issubclass(getattr(obj, nested), dj.Part):
                     setattr(getattr(obj, nested), '_master', obj)
+
+
+class DataJointPlusModule(dj.VirtualModule):
+    """
+    DataJointPlus extension of DataJoint virtual module with the added ability to instantiate from an existing module.
+    """
+    def __init__(self, module_name=None, schema_name=None, module=None, schema_obj_name=None, add_externals=None, add_objects=None, spawn_missing_classes=True, load_dependencies=True, warn=True, dj_kwargs={}):
+        """
+        Add DataJointPlus methods to all DataJoint user tables in a DataJoint virtual module or to an existing module. 
+        
+        To instantiate a DataJoint Virtual Module, provide args module_name and schema_name. 
+        
+        To modify an existing module, provide arg module. 
+        
+        :param module_name (str): displayed module name (if using DataJoint Virtual module)
+        :param schema_name (str): name of the database in mysql
+        :param module (module): module to modify
+        :param schema_obj_name (str): The name of the schema object you wish to instantiate (only needed if the module contains more than one DataJoint dj.schema object)
+        :param add_externals (dict): Dictionary mapping to external files.
+        :param add_objects (dict): additional objects to add to the module
+        :param spawn_missing_classes (bool): Only relevant if module provided. If True, adds DataJoint tables not in module but present in mysql as classes. 
+        :param load_dependencies (bool): Loads the DataJoint graph.
+        :param dj_kwargs (dict): kwargs to pass to dj.VirtualModule (aka dj.create_virtual_module).
+            options: 
+                create_schema - if True, create the schema on the database server
+                create_tables - if True, module.schema can be used as the decorator for declaring new
+                connection - a dj.Connection object to pass into the schema
+        :return: the virtual module or modified module with DataJointPlus added.
+        """
+        if schema_name:
+            assert not module, 'Provide either schema_name or module but not both.'
+            super().__init__(module_name=module_name if module_name else schema_name, schema_name=schema_name, add_objects=add_objects, **dj_kwargs)
+            
+            if load_dependencies:
+                self.__dict__['schema'].connection.dependencies.load()
+            
+        elif module:
+            super(dj.VirtualModule, self).__init__(name=module.__name__)
+            if module_name:
+                if warn:
+                    logging.warning('module_name ignored when instantiated with module.')
+                
+            if schema_obj_name:
+                assert schema_obj_name in module.__dict__, f'schema_obj_name: {schema_obj_name} not found in module.'
+                schema_obj = module.__dict__[schema_obj_name]
+                assert isinstance(schema_obj, dj.Schema), f'schema object should be of type {dj.Schema} not {type(schema_obj)}.'
+            else:
+                schemas = {k: {'obj': v, 'database': v.database} for k, v in module.__dict__.items() if isinstance(v, dj.Schema)}
+                assert len(schemas.keys())==1, f"Found multiple schema objects with names {list(schemas.keys())}, mapping to respective databases {[v['database'] for v in schemas.values()]}. Specify the name of the schema object to instantiate with arg schema_obj_name." 
+                schema_obj = list(schemas.values())[0]['obj']
+            
+            self.__dict__.update(module.__dict__)
+            self.__dict__['schema'] = schema_obj
+            
+            if spawn_missing_classes:
+                schema_obj.spawn_missing_classes(context=self.__dict__)
+                
+            if load_dependencies:
+                schema_obj.connection.dependencies.load()
+                
+            if add_objects:
+                self.__dict__.update(add_objects)
+        
+        else:
+            raise ValueError('Provide schema_name or module.')      
+        
+        if add_externals:
+            register_externals(add_externals)
+            
+        add_datajoint_plus(self)
+    
+create_djp_module = DataJointPlusModule
